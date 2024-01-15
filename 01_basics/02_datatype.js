48ede89c522c8ab17675893456fe4d77fdf254ba
Avinash roy <48274437+Hummer97@users.noreply.github.com>
2024-01-15 17:44:56 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,21 @@
+"use strict"; //this is used to show we are use or treated our code as newer version of JavaScript
+
+//types of Datatype:
+let name = "Avinash Roy" // here, the name variable store string types of data so the "String" is datatype for the name variable.
+let Age = 30 // similarly here, age variable store integer type data. so this variable's datatype is int.
+let isMarride = false // and this variable data type is boolean.
+
+
+//number => 2 to the power 53
+//bigInt
+//String => ""
+//boolean => true/false
+//null => standalone value
+//undefined 
+//symbol => unique 
+
+//object
+
+console.log(typeof "Avinash") //String
+console.log(typeof undefined) //undefined 
+console.log(typeof null) //object
