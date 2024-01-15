f4bbadfc901751e095b6b44d77ca181dc86c58dd
Avinash roy <48274437+Hummer97@users.noreply.github.com>
2024-01-15 19:54:12 +0000

    Learn about dataType conversion and operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -73,3 +73,29 @@ let someValue = undefined
 let convertInString = String(someValue)
 console.log(convertInString);
 console.log(typeof convertInString);
+
+
+//###########################Operators############################
+//Arithmetic Operator 
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+console.log(1+2); //3
+console.log("1"+2); //12
+console.log(1+"2"); //12
+console.log(1+"2" +2); //122
+console.log(1+2+"2"); //32
+let str1 = "Avinash"
+let str2 = " Roy"
+let str3 = str1+str2 // concetination
+console.log(str3); 
+
+let value = 30
+value++; //post increment
+console.log(value); //31
+++value; //pre increment
+console.log(value); //32
