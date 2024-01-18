017739c07a86e849ca6834521f64e7ef1ce2fdae
Avinash roy <48274437+Hummer97@users.noreply.github.com>
2024-01-18 18:53:25 +0000

    Learn about stack and heap

--- 01_basics/datatype_summery.js
+++ 01_basics/datatype_summery.js
@@ -33,3 +33,29 @@ const objetList = {
 const myFunction = function(){
     console.log("This is my function");
 } //function
+
+// ++++++++++++++++++++++++++About Memory+++++++++++++++++++++++++++++++++++++
+//There are two types of memory are used:
+//1. Stack : all the premitive dataType are stored in stack and stack always given copy of the data whenever we want to use that data.
+//2.Heap : Other hand no premitive dataType are stored in heap and whenever we want to use the data it give the refrence of the original data.
+
+//Stack example:
+let myName = "Avinash"
+let yourName = myName
+console.log(yourName); //Avinash
+yourName = "Sameer"
+console.log(myName); //Avinash
+console.log(yourName); //Sameer
+//In this variable gives the copy of the original value so there is no change reflecting on the original value.
+
+//Heap example:
+let userOne = {
+    email : "avinash@gmail.com",
+    code : "IN",
+}
+console.log(userOne.email); //avinash@gmail.com
+let userTwo = userOne
+userTwo.email = "Ramesh@gmail.com"
+console.log(userOne.email); //Ramesh@gmail.com
+console.log(userTwo.email);//Ramesh@gmail.com
+// In this case variable gives the original refrence of the data so whenever we change the value it changed into original reference value.
