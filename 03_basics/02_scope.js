a98af452347c8e4ca016bc26981446bc0c63ee0b
Avinash roy <48274437+Hummer97@users.noreply.github.com>
2024-02-07 00:27:10 +0530

    Learned more about scope

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -6,11 +6,41 @@ if (true) { //this curly braceses are basically called scope
     let a = 201 // this is called Local scope
     const b = 301
     var c = 2370
-    console.log(a);//201
-    console.log(b);//301
-    console.log(c);//2370
+    // console.log(a);//201
+    // console.log(b);//301
+    // console.log(c);//2370
 }
 
-console.log(a);//20
-console.log(b);//30
-console.log(c); //2370 //beacuse var is not follow the scope rules
+// console.log(a);//20
+// console.log(b);//30
+// console.log(c); //2370 //beacuse var is not follow the scope rules
+
+
+function one() {
+    const userName = "Avinash"
+
+    function two() {
+        const website = "youtube"
+        console.log(userName);//Avinash
+    }
+    // console.log(website);//through error due to scope becuse webiste is a local variable
+    two()
+}
+
+one()
+
+
+addNumber(20)
+function addNumber(number) {
+    console.log(number + 1);//21
+    return number + 1;
+
+}
+
+
+
+// addTwo(30)//Cannot access 'addTwo' before initialization
+// // below this type of declearation not allow use to call the function before decleration
+// const addTwo = function (number) {
+//     return number + 20;
+// }
